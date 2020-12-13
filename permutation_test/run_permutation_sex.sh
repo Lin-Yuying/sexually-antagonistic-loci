e6f690c6d26a7e5869755ac8bf2efa59c46c48f1
Lin-Yuying <linyuying.evolution@gmail.com>
2020-12-12 19:51:12 -0800

    permutation test

--- permutation_test/run_permutation_sex.sh
+++ permutation_test/run_permutation_sex.sh
@@ -1,5 +1,6 @@
 sim=1
-while [ $sim -lt 10001 ]
+times=2
+while [ $sim -lt `$times + 1` ]
 do
     echo "############ Simulation ${sim} ###############" >> pop.perm.log
     python permutation.py pop.txt male_${sim}.txt female_${sim}.txt 
@@ -7,6 +8,7 @@ do
     sim=`expr ${sim} + 1`
 done
 cat pop.snp.*.fst | grep -v CHROM > pop.snp.total.fst
+cat pop.snp.*.fst | grep "CHROM"|uniq > header.txt
 cat header.txt pop.snp.total.fst > pop.snp.fst
-python3 fst2csv.py pop.snp.fst pop.snp.sorted.fst 
-python3 cal_pvalues_lin.py -in_fst pop.snp.sorted.fst -out total.pvalue
+python3 ./fst2csv.py pop.snp.fst pop.snp.sorted.fst 
+python3 ./cal_pvalues.py -in_fst pop.snp.sorted.fst -out pop.pvalue
